32115b98dc084aa500ac3111baa631280728534d
Hoang Giang <hoanggiang24484@yahoo.com>
2018-02-22 11:40:44 +0900

    Add components to MainForm

--- TUTORIALS.MyAlbumEditor/MainForm.Designer.cs
+++ TUTORIALS.MyAlbumEditor/MainForm.Designer.cs
@@ -29,18 +29,26 @@
         private void InitializeComponent()
         {
             this.groupBoxAlbums = new System.Windows.Forms.GroupBox();
-            this.groupBoxPhotographs = new System.Windows.Forms.GroupBox();
             this.buttonOpen = new System.Windows.Forms.Button();
-            this.buttonClose = new System.Windows.Forms.Button();
+            this.groupBoxPhotographs = new System.Windows.Forms.GroupBox();
             this.listBoxPhotos = new System.Windows.Forms.ListBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonAlbumProperties = new System.Windows.Forms.Button();
+            this.buttonPhotoProperties = new System.Windows.Forms.Button();
+            this.tableLayoutPanelPhotoCtrl = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonMoveUp = new System.Windows.Forms.Button();
+            this.buttonMoveDown = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
             this.groupBoxAlbums.SuspendLayout();
             this.groupBoxPhotographs.SuspendLayout();
+            this.tableLayoutPanelPhotoCtrl.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBoxAlbums
             // 
             this.groupBoxAlbums.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxAlbums.Controls.Add(this.buttonAlbumProperties);
             this.groupBoxAlbums.Controls.Add(this.buttonOpen);
             this.groupBoxAlbums.Location = new System.Drawing.Point(12, 12);
             this.groupBoxAlbums.Name = "groupBoxAlbums";
@@ -49,10 +57,22 @@
             this.groupBoxAlbums.TabStop = false;
             this.groupBoxAlbums.Text = "&Albums";
             // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpen.Location = new System.Drawing.Point(182, 19);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(80, 23);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "&Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
             // groupBoxPhotographs
             // 
             this.groupBoxPhotographs.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxPhotographs.Controls.Add(this.tableLayoutPanelPhotoCtrl);
             this.groupBoxPhotographs.Controls.Add(this.listBoxPhotos);
             this.groupBoxPhotographs.Location = new System.Drawing.Point(12, 79);
             this.groupBoxPhotographs.Name = "groupBoxPhotographs";
@@ -61,15 +81,19 @@
             this.groupBoxPhotographs.TabStop = false;
             this.groupBoxPhotographs.Text = "&Photographs";
             // 
-            // buttonOpen
+            // listBoxPhotos
             // 
-            this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonOpen.Location = new System.Drawing.Point(285, 19);
-            this.buttonOpen.Name = "buttonOpen";
-            this.buttonOpen.Size = new System.Drawing.Size(69, 23);
-            this.buttonOpen.TabIndex = 0;
-            this.buttonOpen.Text = "&Open";
-            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.listBoxPhotos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxPhotos.FormattingEnabled = true;
+            this.listBoxPhotos.Location = new System.Drawing.Point(6, 19);
+            this.listBoxPhotos.Name = "listBoxPhotos";
+            this.listBoxPhotos.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxPhotos.Size = new System.Drawing.Size(256, 108);
+            this.listBoxPhotos.TabIndex = 0;
+            this.listBoxPhotos.SelectedIndexChanged += new System.EventHandler(this.listBoxPhotos_SelectedIndexChanged);
+            this.listBoxPhotos.DoubleClick += new System.EventHandler(this.listBoxPhotos_DoubleClick);
             // 
             // buttonClose
             // 
@@ -82,16 +106,80 @@
             this.buttonClose.UseVisualStyleBackColor = true;
             this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
             // 
-            // listBoxPhotos
+            // buttonAlbumProperties
             // 
-            this.listBoxPhotos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.listBoxPhotos.FormattingEnabled = true;
-            this.listBoxPhotos.Location = new System.Drawing.Point(6, 19);
-            this.listBoxPhotos.Name = "listBoxPhotos";
-            this.listBoxPhotos.Size = new System.Drawing.Size(256, 108);
-            this.listBoxPhotos.TabIndex = 0;
+            this.buttonAlbumProperties.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAlbumProperties.Location = new System.Drawing.Point(271, 19);
+            this.buttonAlbumProperties.Name = "buttonAlbumProperties";
+            this.buttonAlbumProperties.Size = new System.Drawing.Size(80, 23);
+            this.buttonAlbumProperties.TabIndex = 1;
+            this.buttonAlbumProperties.Text = "Properti&es";
+            this.buttonAlbumProperties.UseVisualStyleBackColor = true;
+            this.buttonAlbumProperties.Click += new System.EventHandler(this.buttonAlbumProperties_Click);
+            // 
+            // buttonPhotoProperties
+            // 
+            this.buttonPhotoProperties.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPhotoProperties.Enabled = false;
+            this.buttonPhotoProperties.Location = new System.Drawing.Point(3, 90);
+            this.buttonPhotoProperties.Name = "buttonPhotoProperties";
+            this.buttonPhotoProperties.Size = new System.Drawing.Size(80, 23);
+            this.buttonPhotoProperties.TabIndex = 2;
+            this.buttonPhotoProperties.Text = "Propertie&s";
+            this.buttonPhotoProperties.UseVisualStyleBackColor = true;
+            this.buttonPhotoProperties.Click += new System.EventHandler(this.buttonPhotoProperties_Click);
+            // 
+            // tableLayoutPanelPhotoCtrl
+            // 
+            this.tableLayoutPanelPhotoCtrl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanelPhotoCtrl.ColumnCount = 1;
+            this.tableLayoutPanelPhotoCtrl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelPhotoCtrl.Controls.Add(this.buttonRemove, 0, 2);
+            this.tableLayoutPanelPhotoCtrl.Controls.Add(this.buttonMoveDown, 0, 1);
+            this.tableLayoutPanelPhotoCtrl.Controls.Add(this.buttonMoveUp, 0, 0);
+            this.tableLayoutPanelPhotoCtrl.Controls.Add(this.buttonPhotoProperties, 0, 3);
+            this.tableLayoutPanelPhotoCtrl.Location = new System.Drawing.Point(268, 13);
+            this.tableLayoutPanelPhotoCtrl.Name = "tableLayoutPanelPhotoCtrl";
+            this.tableLayoutPanelPhotoCtrl.RowCount = 4;
+            this.tableLayoutPanelPhotoCtrl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelPhotoCtrl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelPhotoCtrl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelPhotoCtrl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelPhotoCtrl.Size = new System.Drawing.Size(86, 118);
+            this.tableLayoutPanelPhotoCtrl.TabIndex = 1;
+            // 
+            // buttonMoveUp
+            // 
+            this.buttonMoveUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonMoveUp.Enabled = false;
+            this.buttonMoveUp.Location = new System.Drawing.Point(3, 3);
+            this.buttonMoveUp.Name = "buttonMoveUp";
+            this.buttonMoveUp.Size = new System.Drawing.Size(80, 23);
+            this.buttonMoveUp.TabIndex = 3;
+            this.buttonMoveUp.Text = "Move &Up";
+            this.buttonMoveUp.UseVisualStyleBackColor = true;
+            // 
+            // buttonMoveDown
+            // 
+            this.buttonMoveDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonMoveDown.Enabled = false;
+            this.buttonMoveDown.Location = new System.Drawing.Point(3, 32);
+            this.buttonMoveDown.Name = "buttonMoveDown";
+            this.buttonMoveDown.Size = new System.Drawing.Size(80, 23);
+            this.buttonMoveDown.TabIndex = 4;
+            this.buttonMoveDown.Text = "Move &Down";
+            this.buttonMoveDown.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRemove.Enabled = false;
+            this.buttonRemove.Location = new System.Drawing.Point(3, 61);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(80, 23);
+            this.buttonRemove.TabIndex = 5;
+            this.buttonRemove.Text = "&Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -106,6 +194,7 @@
             this.Text = "MyAlbumEditor";
             this.groupBoxAlbums.ResumeLayout(false);
             this.groupBoxPhotographs.ResumeLayout(false);
+            this.tableLayoutPanelPhotoCtrl.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -117,6 +206,12 @@
         private System.Windows.Forms.GroupBox groupBoxPhotographs;
         private System.Windows.Forms.ListBox listBoxPhotos;
         private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonAlbumProperties;
+        private System.Windows.Forms.Button buttonPhotoProperties;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelPhotoCtrl;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonMoveDown;
+        private System.Windows.Forms.Button buttonMoveUp;
     }
 }
 
--- TUTORIALS.MyAlbumEditor/MainForm.cs
+++ TUTORIALS.MyAlbumEditor/MainForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TUTORIALS.Library;
+using TUTORIALS.Library.Forms;
 
 namespace TUTORIALS.MyAlbumEditor
 {
@@ -31,6 +32,107 @@ namespace TUTORIALS.MyAlbumEditor
             Close();
         }
 
-        
+        private void CloseAlbum()
+        {
+            if(_album.IsChanged)
+            {
+                var dlgAns = MessageBox.Show(
+                    string.Format("Do you want to save your changes to {0}?", _album.FileName), "Save Changes?",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if(dlgAns == DialogResult.Yes)
+                    _album.Save();
+            }
+
+            _album = new PhotoAlbum();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            CloseAlbum();
+            base.OnClosing(e);
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            CloseAlbum();
+
+            using (var openDlg = new OpenFileDialog())
+            {
+                openDlg.Title = "Open Album";
+                openDlg.Filter = "abm files (*.abm)|*.abm| All Files (*.*)|*.*";
+                openDlg.InitialDirectory = PhotoAlbum.DefaultDir;
+
+                try
+                {
+                    if(openDlg.ShowDialog() == DialogResult.OK)
+                    {
+                        _album.Open(openDlg.FileName);
+                        _album.ResetAlbumChanged();
+                        Text = _album.FileName;
+                        UpdateList();
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show(string.Format("Unable to open album: {0}", openDlg.FileName), "Open Album Error",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void UpdateList()
+        {
+            //listBoxPhotos.DataSource = _album;
+            listBoxPhotos.BeginUpdate();
+            listBoxPhotos.Items.Clear();
+            foreach (var p in _album)
+                listBoxPhotos.Items.Add(p);
+            listBoxPhotos.EndUpdate();
+        }
+
+        private void buttonAlbumProperties_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new AlbumEditDlg(_album))
+            {
+                if(dlg.ShowDialog() == DialogResult.OK)
+                {
+                    UpdateList();
+                }
+            }
+        }
+
+        private void buttonPhotoProperties_Click(object sender, EventArgs e)
+        {
+            if (_album.Count == 0) return;
+
+            if (listBoxPhotos.SelectedIndex >= 0)
+                _album.CurrentPosition = listBoxPhotos.SelectedIndex;
+
+            using (var dlg = new PhotoEditDlg(_album))
+            {
+                if(dlg.ShowDialog()==DialogResult.OK)
+                {
+                    UpdateList();
+                }
+            }
+        }
+
+        private void listBoxPhotos_DoubleClick(object sender, EventArgs e)
+        {
+            buttonPhotoProperties.PerformClick();
+        }
+
+        private void listBoxPhotos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var numSelected = listBoxPhotos.SelectedIndices.Count;
+            var someSelected = numSelected > 0;
+
+            buttonMoveUp.Enabled = (someSelected && !listBoxPhotos.GetSelected(0));
+            buttonMoveDown.Enabled = (someSelected && !listBoxPhotos.GetSelected(listBoxPhotos.Items.Count - 1));
+            buttonRemove.Enabled = someSelected;
+            buttonPhotoProperties.Enabled = numSelected == 1;
+        }
+
     }
 }
